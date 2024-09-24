a2d7bbce8e26d561f78c03a673ef04836bd2188e
towhid046 <towhidmorol46@gmail.com>
2024-09-24 11:22:34 +0600

    test

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -11,8 +11,8 @@ const BooksPage = async () => {
       <PageHeader title="Books" url="/books" />
       <section className="container mx-auto px-4 mb-32">
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {books?.map((book) => (
-            <BookCard key={book.number} book={book} />
+          {books?.map((book,index) => (
+            <BookCard key={index} book={book} />
           ))}
         </div>
         <Pagination />
