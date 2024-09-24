b061b1b4443c97206f43dbbe56de08f35f322439
towhid046 <towhidmorol46@gmail.com>
2024-09-24 14:53:53 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/unique/Hero/Hero";
-import PopularAcademicBooks from "@/components/unique/PopularAcademicBooks/PopularAcademicBooks";
-// import OurServices from "@/components/unique/OurServices/OurServices";
+// import PopularAcademicBooks from "@/components/unique/PopularAcademicBooks/PopularAcademicBooks";
+import OurServices from "@/components/unique/OurServices/OurServices";
 // import LectureSheets from "@/components/unique/LectureSheets/LectureSheets";
 // import StationeriesSection from "@/components/unique/StationeriesSection/StationeriesSection";
 // import NonFictionalBooks from "@/components/unique/NonFictionalBooks/NonFictionalBooks";
