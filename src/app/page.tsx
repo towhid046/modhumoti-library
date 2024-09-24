01d9619ec9037db3196dd31cba6f2abf8066a3af
towhid046 <towhidmorol46@gmail.com>
2024-09-24 14:48:59 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 import Hero from "@/components/unique/Hero/Hero";
 import PopularAcademicBooks from "@/components/unique/PopularAcademicBooks/PopularAcademicBooks";
-import OurServices from "@/components/unique/OurServices/OurServices";
-import LectureSheets from "@/components/unique/LectureSheets/LectureSheets";
-import StationeriesSection from "@/components/unique/StationeriesSection/StationeriesSection";
-import NonFictionalBooks from "@/components/unique/NonFictionalBooks/NonFictionalBooks";
-import FictionalBooks from "@/components/unique/FictionalBooks/FictionalBooks";
-import FaqSection from "@/components/unique/FaqSection/FaqSection";
-import ContactSection from "@/components/unique/ContactSection/ContactSection";
+// import OurServices from "@/components/unique/OurServices/OurServices";
+// import LectureSheets from "@/components/unique/LectureSheets/LectureSheets";
+// import StationeriesSection from "@/components/unique/StationeriesSection/StationeriesSection";
+// import NonFictionalBooks from "@/components/unique/NonFictionalBooks/NonFictionalBooks";
+// import FictionalBooks from "@/components/unique/FictionalBooks/FictionalBooks";
+// import FaqSection from "@/components/unique/FaqSection/FaqSection";
+// import ContactSection from "@/components/unique/ContactSection/ContactSection";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <OurServices />
-      <PopularAcademicBooks />
-      <LectureSheets />
+      {/* <PopularAcademicBooks /> */}
+      {/* <LectureSheets />
       <StationeriesSection />
       <NonFictionalBooks />
       <FictionalBooks />
       <FaqSection />
-      <ContactSection />
+      <ContactSection /> */}
     </>
   );
 };
