42fef9631df11a2a4f0670f597ce219953a8c621
towhid046 <towhidmorol46@gmail.com>
2024-09-27 22:05:42 +0200

    replace some meaningfull text in our services section in home page

--- src/components/unique/OurServices/OurServices.tsx
+++ src/components/unique/OurServices/OurServices.tsx
@@ -1,9 +1,15 @@
 import SectionHeader from "@/components/shared/SectionHeader/SectionHeader";
-import Image from "next/image";
-const services = [1, 2, 3, 4, 5, 6];
-import bookLogo from "../../../assets/images/book-logo.png";
+interface Service {
+  title: string;
+  description: string;
+  icon: string;
+}
+const OurServices = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/services.json`
+  );
+  const services:Service[] = await res.json();
 
-const OurServices = () => {
   return (
     <section className="container mx-auto px-4 mb-32">
       <SectionHeader
@@ -13,28 +19,17 @@ const OurServices = () => {
         urlLabel="View Books"
       />
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 ">
-        {services?.map((service) => (
+        {services?.map((service,index) => (
           <div
-            key={service}
+            key={index}
             className="hover:shadow-lg transition duration-500 rounded-md space-y-7 p-5 bg-base-100 shadow-sm "
           >
-            <figure className="flex justify-center">
-              <Image
-                width={100}
-                height={100}
-                src={bookLogo || ""}
-                alt="Image"
-                className="object-cover"
-              />
-            </figure>
+            <figure className="flex justify-center text-4xl">{service.icon}</figure>
             <div className="space-y-3 text-center">
               <h3 className="lg:text-3xl text-gray-700 text-2xl font-semibold">
-                Research Binding
+                {service.title}
               </h3>
-              <p className="text-gray-500">
-                Replace this text with some meaningful text in later. Replace
-                this text with some meaningful text in later{" "}
-              </p>
+              <p className="text-gray-500">{service.description}</p>
             </div>
           </div>
         ))}
