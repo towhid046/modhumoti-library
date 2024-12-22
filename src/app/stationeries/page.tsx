39a2d93e79e17033e03966a2550a737ed2994673
towhid046 <towhidmorol46@gmail.com>
2024-12-22 16:07:28 +0600

    polish the stationary cards and remove the view more butto from contact section

--- src/app/stationeries/page.tsx
+++ src/app/stationeries/page.tsx
@@ -2,7 +2,7 @@ import PageHeader from "@/components/shared/PageHeader/PageHeader";
 import Image from "next/image";
 import Button from "@/components/shared/Button/Button";
 import Pagination from "@/components/unique/Pagination/Pagination";
-const stationeries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15];
+const stationeries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 const image = `https://images.pexels.com/photos/254717/pexels-photo-254717.jpeg?auto=compress&cs=tinysrgb&w=600`;
 const StationeriesPage = () => {
   return (
@@ -23,22 +23,22 @@ const StationeriesPage = () => {
               </figure>
               <div className="space-y-.5">
                 <div className='flex justify-between flex-warp gap-1'>
-                <h2 className="text-2xl font-bold">
-                  Pencil 
-                </h2>
-                <span className="text-xs font-normal bg-green-300 py-[3px] px-2 text-white flex items-center justify-center rounded-full ">2 pics</span>
+                  <h2 className="text-2xl font-bold">
+                    Pencil
+                  </h2>
+                  <span className="text-xs font-normal bg-green-300 py-[3px] px-2 text-white flex items-center justify-center rounded-full ">2 pics</span>
                 </div>
                 <h3 className="text-lg font-semibold text-primary-color">
                   Price: 15 BDT
                 </h3>
                 <p>One pics pen 5 Tk</p>
               </div>
-                <Button customClass="w-full">Add to Cart</Button>
+              {/* <Button customClass="w-full">Add to Cart</Button> */}
             </div>
           ))}
         </div>
         <div>
-          <Pagination/>
+          <Pagination />
         </div>
       </section>
     </>
